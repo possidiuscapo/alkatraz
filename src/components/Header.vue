b40847e4c15a19b6e3a6d643650f102268090f96
Possidius Capo <possidiusc@gmail.com>
2023-07-14 18:58:10 +0100

    première application vuejs

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<script>
+export default{
+
+}
+</script>
+ <template>
+    <div>LOGO</div>
+    <ul>
+        <li><a href="">Lorem.</a></li>
+        <li><a href="">Officia.</a></li>
+        <li><a href="">Assumenda.</a></li>
+    </ul>
+ </template>
